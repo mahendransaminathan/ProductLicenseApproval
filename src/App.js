ccba213800a1fc00962cbd9368fcfcdf37855c3f
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-04 20:28:39 +0000

    Updated for Responsive Design

--- src/App.js
+++ src/App.js
@@ -61,64 +61,98 @@ function FormPage() {
 
   return (
     
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         
-        <form className='form-container' onSubmit={handleSubmit}>
-      
+        <form className='space-y-4' onSubmit={handleSubmit}>
+        
           <label htmlFor="Heading" className="heading-label">
             Submit your Details for License Approval
           </label>
           <div className='form-group'>
+            <div className='form-item'>
+              <label htmlFor="firstname">First Name*</label>
+              <input
+                type="text"
+                id="firstname"
+                value={firstname}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder='Enter Your First Name'
+              />
+            </div>
+            <div className='form-item'>
+              <label htmlFor="country">Country</label>
+              <select
+                id="country"
+                name="selectedCountry"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              >
+                <option value="Select a Country">Select a Country</option>
+                <option value="UnitedStates">United States</option>
+                <option value="UnitedKingdom">United Kingdom</option>
+                <option value="Australia">Australia</option>
+                <option value="Germany">Germany</option>
+                <option value="France">France</option>
+                <option value="Canada">Canada</option>
+                <option value="Japan">Japan</option>
+              </select>
+            </div>
+          </div>
+
+          <div className='form-group'>
+            <div className='form-item'>
+              <label htmlFor="lastname">Last Name*</label>                
+              <input type="text" id="lastname" value={lastname} 
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder='Enter Your Last Name'/>
+            </div>
+            <div className='form-item'>              
+              <label htmlFor="emailID">EmailID*</label>
+              <input type="text" id="emailID" value={emailID} 
+              onChange={(e) => setEmailID(e.target.value)} 
+              placeholder='Enter Your EmailID'/>
             
-            <label htmlFor="firstname">First Name*</label>
-            <input type="text" id="firstname" value={firstname} 
-            onChange={(e) => setFirstName(e.target.value)} 
-            placeholder='Enter First Your Name'/>
-            <label htmlFor="lastname">Last Name*</label>
-            <input type="text" id="lastname" value={lastname} 
-            onChange={(e) => setLastName(e.target.value)} 
-            placeholder='Enter Your Last Name'/>
+            </div>
+          </div>
+          <div className='form-group'>
+            <div className='form-item'>
             <label htmlFor="addressline1">Address Line1</label>
             <input type="text" id="addressline1" value={addressline1} 
             onChange={(e) => setAddressLine1(e.target.value)} 
             placeholder='Enter the Address Line'/>
+            </div>
+            <div className='form-item'>
+            <label htmlFor="phoneNumber">Phone Number</label>            
+            <input type="text" id="phoneNumber" value={phoneNumber} 
+            onChange={(e) => setPhoneNumber(e.target.value)} 
+            placeholder='Enter Your Phone Numbr'/>
+            </div>
+          </div>
+          
+          <div className='form-group'>
+            <div className='form-item'>
+
             <label htmlFor="addressline2">Address Line2</label>
             <input type="text" id="addressline2" value={addressline2} 
             onChange={(e) => setAddressLine2(e.target.value)} 
             placeholder='Enter the address line2'/>
-              <label htmlFor="city">City</label>
+            </div>
+            <div className='form-item'>
+            <label htmlFor="city">City</label>
             <input type="text" id="City" value={city} 
             onChange={(e) => setCity(e.target.value)} 
             placeholder='Enter Your city'/>
+            </div>
+            <div className='form-item'>
             <label htmlFor="EirCode">Eircode</label>
             <input type="text" id="eircode" value={eircode} 
             onChange={(e) => setEirCode(e.target.value)} 
             placeholder='Enter the EirCode'/>
-               
+            </div>   
           </div>
-          <div className='form-group'>
                   
-          <label htmlFor="country">Country</label>
-            <select id="country" name="selectedCountry" value={country} onChange={(e) => setCountry(e.target.value)}>
-              <option value="">Select a Country</option>
-              <option value="UnitedStates">United States</option>
-              <option value="UnitedKingdom">United Kingdom</option>
-              <option value="Australia">Australia</option>
-              <option value="Germany">Germany</option>
-              <option value="France">France</option>
-              <option value="Canada">Canada</option>
-              <option value="Japan">Japan</option>
-            </select>  
-            <label htmlFor="emailID">EmailID*</label>
-            <input type="text" id="emailID" value={emailID} 
-            onChange={(e) => setEmailID(e.target.value)} 
-            placeholder='Enter Your EmailID'/>
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <input type="text" id="phoneNumber" value={phoneNumber} 
-            onChange={(e) => setPhoneNumber(e.target.value)} 
-            placeholder='Enter Your Phone Numbr'/>
-          </div>
+             
           <div class="button-container">
             <button type="submit">Submit</button>          
           </div>
